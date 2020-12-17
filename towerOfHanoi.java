2b7c0e9ddeebd5f18124e3804c05c139b65a5b65
HarshalHattyangdi <harshal.2797@gmail.com>
2020-12-17 19:23:44 +0530

    Completed tower of hanoi program completely

--- towerOfHanoi.java
+++ towerOfHanoi.java
@@ -19,6 +19,8 @@ public class towerOfHanoi {
         source = ob.createStack(num_of_disk);
         destination = ob.createStack(num_of_disk);
         auxiliary = ob.createStack(num_of_disk);
+
+        ob.toh(num_of_disk, source, destination, auxiliary);
     }
 
     class Stack {
@@ -40,7 +42,7 @@ public class towerOfHanoi {
     }
 
     boolean isEmpty(Stack stack) {
-        return (stack.top == 1);
+        return (stack.top == -1);
     }
 
     void push(Stack stack, int item) {
@@ -55,6 +57,68 @@ public class towerOfHanoi {
             return Integer.MIN_VALUE;
         }
         return stack.array[stack.top--];
+    }
+
+    void moveDisksBetween(Stack src, Stack dest, char s, char d) {
+        int pole1TopDisk = pop(src);
+        int pole2TopDisk = pop(dest);
+
+        // When pole1 is empty
+        if (pole1TopDisk == Integer.MIN_VALUE) {
+            push(src, pole2TopDisk);
+            moveDisk(d, s, pole2TopDisk);
+        }
+        // When pole2 is empty
+        else if (pole2TopDisk == Integer.MIN_VALUE) {
+            push(dest, pole1TopDisk);
+            moveDisk(s, d, pole1TopDisk);
+        }
+        // When top disk of pole1>top disk of pole2
+        else if (pole1TopDisk > pole2TopDisk) {
+            push(src, pole1TopDisk);
+            push(src, pole2TopDisk);
+            moveDisk(d, s, pole2TopDisk);
+        }
+        // When top disk of pole1 < top disk of pole2
+        else {
+            push(dest, pole2TopDisk);
+            push(dest, pole1TopDisk);
+            moveDisk(s, d, pole1TopDisk);
+        }
+    }
+
+    void moveDisk(char fromPeg, char toPeg, int disk) {
+        System.out.println("Move the disk " + disk + " from " + fromPeg + " to " + toPeg);
+    }
+
+    void toh(int num_of_disk, Stack src, Stack dest, Stack aux) {
+        // Calculate total number of steps required
+        int num_of_steps = (int) (Math.pow(2, num_of_disk) - 1);
+
+        char s = 'S', a = 'A', d = 'D';
+
+        // When number of disk is even then interchange the auxiliary pole and
+        // destination pole.
+        if (num_of_disk % 2 == 0) {
+            char temp = d;
+            d = a;
+            a = temp;
+        }
+
+        // Larger disks need to be pushed first
+        for (int i = num_of_disk; i >= 1; i--) {
+            push(src, i);
+        }
+
+        for (int i = 1; i <= num_of_steps; i++) {
+            if (i % 3 == 1) {
+                moveDisksBetween(src, dest, s, d);
+            } else if (i % 3 == 2) {
+                moveDisksBetween(src, aux, s, a);
+            } else {
+                moveDisksBetween(aux, dest, a, d);
+            }
+        }
     }
 
 }
